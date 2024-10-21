68cf3aac7b4c604ca046a4ce9f7f3011310f0ce0
katerina <katerina@>
2024-10-21 18:35:23 +0300

    KS-1-4 do task4

---
+++ src/main/kotlin/lesson_1/lesson1_task4.kt
@@ -0,0 +1,19 @@
+package ru.fedorova.sprint.lesson_1
+
+fun main() {
+
+    val lenght: Long = 40_868_600_000
+    val age: Byte = 27
+    val partOfDay: Float = 0.075f
+    val seconds: Short = 6480
+    val partOfYear: Double = 2.0547945205479453E-4
+    val apogee: Int = 32700
+
+    println(lenght)
+    println(age)
+    println(partOfDay)
+    println(seconds)
+    println(partOfYear)
+    println(apogee)
+
+}
