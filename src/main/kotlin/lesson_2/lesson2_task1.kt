522ce214d6358a2766fda777e083be3436790625
katerina <katerina@>
2024-10-22 13:16:06 +0300

    KS-2-1 do task1

---
+++ src/main/kotlin/lesson_2/lesson2_task1.kt
@@ -0,0 +1,12 @@
+package ru.fedorova.sprint.lesson_2
+
+fun main() {
+
+    val countStudents: Float = 4.0f
+    val sumPoints: Int = 3 + 4 + 3 + 5
+
+    val averagePoints: Float = sumPoints / countStudents
+
+    println(averagePoints)
+
+}
