7b910648be617df53f0f7ae0f044f7bfa7f9f023
katerina <katerina@>
2024-10-23 14:31:10 +0300

    KS-1-5 refactoring 2: change const

--- src/main/kotlin/lesson_1/lesson1_task5.kt
+++ src/main/kotlin/lesson_1/lesson1_task5.kt
@@ -1,15 +1,15 @@
 package ru.fedorova.sprint.lesson_1
 
 const val SECONDS_IN_MINUTE: Int = 60
-const val SECONDS_IN_HOUR: Int = 3600
+const val MINUTES_IN_HOUR: Int = 60
 
 fun main() {
 
     val countOfSeconds: Int = 6480
 
-    val hours: Int = countOfSeconds / SECONDS_IN_HOUR
-    val minutes: Int = (countOfSeconds - hours * SECONDS_IN_HOUR) / SECONDS_IN_MINUTE
-    val seconds: Int = countOfSeconds - hours * SECONDS_IN_HOUR - minutes * SECONDS_IN_MINUTE
+    val hours: Int = countOfSeconds / (MINUTES_IN_HOUR * SECONDS_IN_MINUTE)
+    val minutes: Int = (countOfSeconds % (MINUTES_IN_HOUR * SECONDS_IN_MINUTE)) / SECONDS_IN_MINUTE
+    val seconds: Int = countOfSeconds % SECONDS_IN_MINUTE
 
     println(String.format("%02d:%02d:%02d", hours, minutes, seconds))
 
