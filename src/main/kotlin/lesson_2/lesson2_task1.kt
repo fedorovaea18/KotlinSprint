ec35a657225c3787c2fdedb7934c7eaf2299b741
katerina <katerina@>
2024-10-23 11:09:11 +0300

    KS-2-1 refactoring

--- src/main/kotlin/lesson_2/lesson2_task1.kt
+++ src/main/kotlin/lesson_2/lesson2_task1.kt
@@ -2,10 +2,10 @@ package ru.fedorova.sprint.lesson_2
 
 fun main() {
 
-    val countStudents: Float = 4.0f
+    val countStudents: Int = 4
     val sumPoints: Int = 3 + 4 + 3 + 5
 
-    val averagePoints: Float = sumPoints / countStudents
+    val averagePoints: Float = sumPoints.toFloat() / countStudents
 
     println(averagePoints)
 
