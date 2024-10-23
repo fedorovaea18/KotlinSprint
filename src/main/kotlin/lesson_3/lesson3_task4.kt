e4ea42f49d6544efe15b6c0dc6fd69b8e130af96
katerina <katerina@>
2024-10-23 18:07:59 +0300

    KS-3-4 feat: аdd to print of moving chess pieces with number of moves

---
+++ src/main/kotlin/lesson_3/lesson3_task4.kt
@@ -0,0 +1,16 @@
+package ru.fedorova.sprint.lesson_3
+
+fun main() {
+
+    var stepFrom: String = "E2"
+    var stepTo: String = "E4"
+    var numberOfStep: Int = 1
+
+    println("$stepFrom-$stepTo;$numberOfStep")
+
+    stepFrom = "D2"
+    stepTo = "D3"
+    numberOfStep = 2
+    println("$stepFrom-$stepTo;$numberOfStep")
+
+}
