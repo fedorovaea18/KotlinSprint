67464b2d130b2b4c204d314ff2f3e7fff54cd0ee
katerina <katerina@>
2024-10-23 11:14:48 +0300

    KS-2-3 refactoring

--- src/main/kotlin/lesson_2/lesson2_task3.kt
+++ src/main/kotlin/lesson_2/lesson2_task3.kt
@@ -1,5 +1,7 @@
 package ru.fedorova.sprint.lesson_2
 
+const val MINUTES_IN_HOUR: Int = 60
+
 fun main() {
 
     val departureHour: Int = 9
@@ -7,11 +9,11 @@ fun main() {
 
     val travelTimeInMinutes: Int = 457
 
-    val totalDepartureMinutes: Int = departureHour * 60 + departureMinute
+    val totalDepartureMinutes: Int = departureHour * MINUTES_IN_HOUR + departureMinute
     val arrivalMinutes: Int = totalDepartureMinutes + travelTimeInMinutes
 
-    val arrivalHour: Int = arrivalMinutes / 60
-    val arrivalMinute: Int = arrivalMinutes % 60
+    val arrivalHour: Int = arrivalMinutes / MINUTES_IN_HOUR
+    val arrivalMinute: Int = arrivalMinutes % MINUTES_IN_HOUR
 
     print(arrivalHour)
     print(":")
