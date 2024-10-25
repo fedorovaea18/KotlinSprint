4b908b7fc236636f41b9862b99acf8e6cd411957
fedorovaea18 <fedorovaea18@yandex.ru>
2024-10-25 17:29:06 +0300

    KS-4-3 fix: added const val

--- src/main/kotlin/lesson_4/lesson4_task3.kt
+++ src/main/kotlin/lesson_4/lesson4_task3.kt
@@ -1,13 +1,18 @@
 package ru.fedorova.sprint.lesson_4
 
+const val IS_SUNNY: Boolean= true
+const val IS_TENT_OPEN: Boolean = true
+const val REQUIRED_HUMIDITY: Int = 20
+const val BAD_SEASON: String = "зима"
+
 fun main() {
 
-    val isSunny: Boolean = true
-    val isTentOpen: Boolean = true
-    val humidity: Int = 20
-    val currentSeason: String = "winter"
+    val isSunny = true
+    val isTentOpen = true
+    val humidity = 20
+    val currentSeason = "зима"
 
-    val goodConditions: Boolean = isSunny && isTentOpen && (humidity == 20) && (currentSeason != "winter")
+    val goodConditions = (isSunny == IS_SUNNY) && (isTentOpen == IS_TENT_OPEN) && (humidity == REQUIRED_HUMIDITY) && (currentSeason != BAD_SEASON)
 
     println("Благоприятные ли условия сейчас для роста бобовых? $goodConditions")
 
