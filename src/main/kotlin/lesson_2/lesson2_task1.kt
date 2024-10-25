16c3305e89b38d031812aea27c2472d23f98d011
katerina <katerina@>
2024-10-25 09:46:28 +0300

    KS-2-1 fix magic numbers

--- src/main/kotlin/lesson_2/lesson2_task1.kt
+++ src/main/kotlin/lesson_2/lesson2_task1.kt
@@ -3,7 +3,13 @@ package ru.fedorova.sprint.lesson_2
 fun main() {
 
     val countStudents: Int = 4
-    val sumPoints: Int = 3 + 4 + 3 + 5
+
+    val point1: Int = 3
+    val point2: Int = 4
+    val point3: Int = 3
+    val point4: Int = 5
+
+    val sumPoints: Int = point1 + point2 + point3 + point4
 
     val averagePoints: Float = sumPoints.toFloat() / countStudents
 
