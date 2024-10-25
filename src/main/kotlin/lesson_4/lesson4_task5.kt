07b9d69d9b1ee3c352546c044c624c42cc038f38
fedorovaea18 <fedorovaea18@yandex.ru>
2024-10-25 16:54:09 +0300

    KS-4-5 fetch: added a function of ship checking for sending to sail

---
+++ src/main/kotlin/lesson_4/lesson4_task5.kt
@@ -0,0 +1,30 @@
+package ru.fedorova.sprint.lesson_4
+
+const val MIN_CREW_COUNT = 55
+const val MAX_CREW_COUNT = 70
+const val REQUIRED_CREW_FOR_DAMAGE = 70
+const val MIN_FOOD_BOXES = 50
+
+fun main() {
+
+    println("Наличие повреждений корпуса (true/false)")
+    val hasDamage = readLine()!!.toBoolean()
+
+    println("Текущий состав экипажа")
+    val crewCount = readLine()!!.toInt()
+
+    println("Количество ящиков с провизией на борту")
+    val foodBoxes = readLine()!!.toInt()
+
+    println("Благоприятность метеоусловий (true/false)")
+    val isWeatherGood = readLine()!!.toBoolean()
+
+    if (!hasDamage && (crewCount >= MIN_CREW_COUNT && crewCount <= MAX_CREW_COUNT) && foodBoxes > MIN_FOOD_BOXES) {
+        println("Корабль может отправиться в долгосрочное плавание")
+    } else if (hasDamage && crewCount == REQUIRED_CREW_FOR_DAMAGE && isWeatherGood && foodBoxes >= MIN_FOOD_BOXES) {
+        println("Корабль может отправиться в плавание")
+    } else {
+        println("Корабль не может приступить к плаванию")
+    }
+
+}
