160e8f1dade8c486365b48d5af6bcba5d11c9fea
fedorovaea18 <fedorovaea18@yandex.ru>
2024-10-26 11:51:37 +0300

    KS-2-4 fix: added const FULL_PERCENTAGE

--- src/main/kotlin/lesson_2/lesson2_task4.kt
+++ src/main/kotlin/lesson_2/lesson2_task4.kt
@@ -1,5 +1,6 @@
 package ru.fedorova.sprint.lesson_2
 
+const val FULL_PERCENTAGE = 100
 
 fun main() {
 
@@ -7,8 +8,8 @@ fun main() {
     val ironOre: Int = 11
     val buff: Int = 20
 
-    val buffOfCrystalOre: Int = (crystalOre * buff / 100)
-    val bufOfIronOre: Int = (ironOre * buff / 100)
+    val buffOfCrystalOre: Int = (crystalOre * buff / FULL_PERCENTAGE)
+    val bufOfIronOre: Int = (ironOre * buff / FULL_PERCENTAGE)
 
     println(buffOfCrystalOre)
     println(bufOfIronOre)
