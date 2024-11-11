8db8f1aa59fdbf3a89cce921d0e799d68a3fc6c7
fedorovaea18 <fedorovaea18@yandex.ru>
2024-11-11 19:44:17 +0300

    KS-6-2 feat: improved the timer function

---
+++ src/main/kotlin/lesson_6/lesson6_task3.kt
@@ -0,0 +1,16 @@
+package ru.fedorova.spring.lesson_6
+
+fun main() {
+
+    println("Введите количество секунд")
+    var seconds = readln().toInt()
+
+    while (seconds > 0) {
+        println("Осталось секунд: $seconds")
+        Thread.sleep(1000)
+        seconds--
+    }
+
+    println("Время вышло")
+
+}
