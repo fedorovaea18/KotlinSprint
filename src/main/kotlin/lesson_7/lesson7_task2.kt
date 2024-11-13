b3ed98d5bb5c9e38692823c849547f86781a5b98
fedorovaea18 <fedorovaea18@yandex.ru>
2024-11-13 18:13:23 +0300

    KS-7-2 feat: added the authorization function by generated SMS code

---
+++ src/main/kotlin/lesson_7/lesson7_task2.kt
@@ -0,0 +1,21 @@
+package ru.fedorova.spring.lesson_7
+
+fun main() {
+
+    while(true) {
+
+        val code = (1000..9999).random()
+        println("Ваш код авторизации: $code")
+
+        println("Введите код из смс:")
+        val userInput = readln().toInt()
+
+        if (userInput == code) {
+            println("Приветствую!")
+            return
+        } else {
+            println("Код будет выслан снова. Попробуйте ещё раз.")
+        }
+    }
+
+}
