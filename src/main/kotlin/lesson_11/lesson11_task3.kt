406a914cf59568bcd3ab0af6b93f363536d4ea44
fedorovaea18 <fedorovaea18@yandex.ru>
2024-12-12 17:04:02 +0300

    KS-11-3 fix: classes, objects and functions with them are combined into one file

--- src/main/kotlin/lesson_11/lesson11_task3.kt
+++ src/main/kotlin/lesson_11/lesson11_task3.kt
@@ -1,20 +1,50 @@
 package ru.fedorova.spring.lesson_11
 
+class ChatUser(
+    val name: String,
+    val avatar: String,
+    var status: String,
+) {
+
+}
+
+class ChatRoom(
+    val cover: String,
+    val name: String,
+    val users: MutableList<ChatUser>,
+) {
+
+    fun addUser(user: ChatUser) {
+        users.add(user)
+        println("Пользователь ${user.name} добавлен в $name")
+    }
+
+    fun updateUserStatus(username: String, status: String) {
+        users.forEach { user ->
+            if (user.name == username) {
+                user.status = status
+            }
+        }
+        println("Пользователь $username обновил статус на $status")
+    }
+
+}
+
 fun main() {
 
-    val room: Room = Room(
+    val room: ChatRoom = ChatRoom(
         cover = "Картинка комнаты",
         name = "Комната любителей фэнтези",
         users = mutableListOf(),
     )
 
-    val user1: User3 = User3(
+    val user1: ChatUser = ChatUser(
         name = "Маша",
         avatar = "Картинка аватара Маши",
         status = "Разговаривает",
     )
 
-    val user2: User3 = User3(
+    val user2: ChatUser = ChatUser(
         name = "Петя",
         avatar = "Картинка аватара Пети",
         status = "Микрофон выключен",
