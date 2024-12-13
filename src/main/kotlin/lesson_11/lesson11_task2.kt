5b497d013c389d19eede549b4cc0d89fd3b138cc
fedorovaea18 <fedorovaea18@yandex.ru>
2024-12-13 13:18:43 +0300

    KS-11-2 fix: class, object and functions with them are combined into one file

--- src/main/kotlin/lesson_11/lesson11_task2.kt
+++ src/main/kotlin/lesson_11/lesson11_task2.kt
@@ -1,6 +1,41 @@
 package lesson_11
 
-import ru.fedorova.spring.lesson_11.User2
+class User2(
+    val id: Int,
+    val login: String,
+    var password: String,
+    val email: String,
+    var bio: String,
+) {
+
+    fun infoUser() {
+        println("id: $id")
+        println("Логин: $login")
+        println("Пароль: $password")
+        println("Email: $email")
+        println("О себе: $bio")
+    }
+
+    fun changeUserBio() {
+        println("Введите информацию о себе:")
+        val newBio = readln().toString()
+        bio = newBio
+    }
+
+    fun changeUserPassword() {
+        println("Введите текущий пароль:")
+        val currentPassword = readln().toString()
+
+        if (currentPassword == password) {
+            println("Введите новый пароль:")
+            val newPassword = readln().toString()
+            password = newPassword
+            println("Пароль изменен!")
+        }
+
+    }
+
+}
 
 fun main() {
 
