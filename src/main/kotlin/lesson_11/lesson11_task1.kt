f918572d88a7ee6c576cf452cbb5bcbd4a58dcf0
fedorovaea18 <fedorovaea18@yandex.ru>
2024-12-13 13:53:22 +0300

    KS-11-1 fix: deleted file User.kt

--- src/main/kotlin/lesson_11/lesson11_task1.kt
+++ src/main/kotlin/lesson_11/lesson11_task1.kt
@@ -1,5 +1,14 @@
 package ru.fedorova.spring.lesson_11
 
+class User(
+    val id: Int,
+    val login: String,
+    val password: String,
+    val email: String,
+) {
+
+}
+
 fun main() {
 
     val user1: User = User(
