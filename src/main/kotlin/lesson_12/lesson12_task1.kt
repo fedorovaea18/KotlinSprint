c043c4c7d49b4e785b9c7b7f3222f8ef38a68731
fedorovaea18 <fedorovaea18@yandex.ru>
2024-12-16 18:24:10 +0300

    KS-12-1 feat: added weather tracker for days with empty constructor

---
+++ src/main/kotlin/lesson_12/lesson12_task1.kt
@@ -0,0 +1,37 @@
+package ru.fedorova.spring.lesson_12
+
+class Weather() {
+
+    var dayTemperature: Int = 50
+    var nightTemperature: Int = -50
+    var rain: Boolean = false
+
+    fun displayWeather() {
+        println("Дневная температура: $dayTemperature")
+        println("Ночная температура: $nightTemperature")
+        println("Наличие осадков в течение суток: $rain")
+    }
+
+}
+
+fun main() {
+
+    val weather1 = Weather()
+    weather1.dayTemperature = 25
+    weather1.nightTemperature = 20
+    weather1.rain = false
+
+    val weather2 = Weather()
+    weather2.dayTemperature = 10
+    weather2.nightTemperature = 2
+    weather2.rain = true
+
+    println("Погода в первый день")
+    weather1.displayWeather()
+
+    println()
+
+    println("Погода во второй день")
+    weather2.displayWeather()
+
+}
