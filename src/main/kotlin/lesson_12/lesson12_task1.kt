1a088848b62d2c191030228a865d5fa129a989ac
fedorovaea18 <fedorovaea18@yandex.ru>
2024-12-16 21:09:48 +0300

    KS-12-1 fix: changed variable name rain to isRaining

--- src/main/kotlin/lesson_12/lesson12_task1.kt
+++ src/main/kotlin/lesson_12/lesson12_task1.kt
@@ -1,15 +1,15 @@
 package ru.fedorova.spring.lesson_12
 
-class Weather() {
+class Weather{
 
     var dayTemperature: Int = 50
     var nightTemperature: Int = -50
-    var rain: Boolean = false
+    var isRaining: Boolean = false
 
     fun displayWeather() {
         println("Дневная температура: $dayTemperature")
         println("Ночная температура: $nightTemperature")
-        println("Наличие осадков в течение суток: $rain")
+        println("Наличие осадков в течение суток: $isRaining")
     }
 
 }
@@ -19,12 +19,12 @@ fun main() {
     val weather1 = Weather()
     weather1.dayTemperature = 25
     weather1.nightTemperature = 20
-    weather1.rain = false
+    weather1.isRaining = false
 
     val weather2 = Weather()
     weather2.dayTemperature = 10
     weather2.nightTemperature = 2
-    weather2.rain = true
+    weather2.isRaining = true
 
     println("Погода в первый день")
     weather1.displayWeather()
