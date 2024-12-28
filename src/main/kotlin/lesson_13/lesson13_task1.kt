4802efc372f2f8021ced745a1b1eab34a02904fd
fedorovaea18 <fedorovaea18@yandex.ru>
2024-12-28 13:50:52 +0300

    KS-13-1 fix: deleted default value

--- src/main/kotlin/lesson_13/lesson13_task1.kt
+++ src/main/kotlin/lesson_13/lesson13_task1.kt
@@ -1,6 +1,5 @@
 package ru.fedorova.spring.lesson_13
 
-class PhoneBook(val name: String, val company: String? = null, val phoneNumber: Long) {
+class PhoneBook(val name: String, val company: String?, val phoneNumber: Long) {
 
 }
-
