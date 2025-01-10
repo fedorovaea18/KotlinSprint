e44d688d5b1dfa3192f90fd4606ce71edd814be0
fedorovaea18 <fedorovaea18@yandex.ru>
2025-01-10 15:53:45 +0300

    KS-13-3 feat: added filter by companies without null values

--- src/main/kotlin/lesson_13/lesson13_task3.kt
+++ src/main/kotlin/lesson_13/lesson13_task3.kt
@@ -1,28 +1,19 @@
 package ru.fedorova.spring.lesson_13
 
-class PhoneBookThird(val name: String, val company: String?, val phoneNumber: Long) {
+class PhoneBookThird(val name: String, val company: String? = null, val phoneNumber: Long) {
 
 }
 
 fun main() {
 
     val phoneBookContacts = listOf(
-        PhoneBookThird("Антон", null, 89056989487),
-        PhoneBookThird("Елена", null, 89214569887),
+        PhoneBookThird("Антон", phoneNumber = 89056989487),
+        PhoneBookThird("Елена", phoneNumber = 89214569887),
         PhoneBookThird("Иван", "null", 89601234789),
         PhoneBookThird("Мария", "Сбер", 89205673497),
         PhoneBookThird("Анна", "Яндекс", 89623336699)
     )
 
-    phoneBookContacts.forEach { println(it.company) }
+    phoneBookContacts.mapNotNull { it.company }.forEach { println(it) }
 
 }
-
-//Скопируй класс из предыдущей задачи и измени его название для избегания конфликтов.
-//
-//Для того, чтобы взаимодействовать со всеми записями телефонной книги, как с одним целым, их нужно объединить в список.
-//
-//- создай и инициализируй (одновременно) список с 5 объектами класса-контакта;
-//- 2 контакта должны принимать null для поля компании;
-//- 1 контакт должен иметь название компании “null”;
-//- выведи список всех существующих компаний, которые есть в телефонной книге.
