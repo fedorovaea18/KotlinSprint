be50f63d93bf4964c6c4a23151a90dfe0e6d773f
fedorovaea18 <fedorovaea18@yandex.ru>
2025-01-29 12:39:00 +0300

    KS-16-4 feat: added class Order and functions updateStatus() and requestChangeStatus()

---
+++ src/main/kotlin/lesson_16/lesson16_task4.kt
@@ -0,0 +1,16 @@
+package ru.fedorova.spring.lesson_16
+
+class Order(
+    private val orderNumber: Int,
+    private var status: String,
+) {
+
+    private fun updateStatus(newStatus: String) {
+        status = newStatus
+    }
+
+    fun requestChangeStatus(newStatus: String) {
+        updateStatus(newStatus)
+    }
+
+}
