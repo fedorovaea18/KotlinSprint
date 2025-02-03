8abfce5753bdd6ca5dd6b191678f40dca1c4c263
fedorovaea18 <fedorovaea18@yandex.ru>
2025-02-03 21:21:50 +0300

    KS-18-2 fix:

--- src/main/kotlin/lesson_18/lesson18_task2.kt
+++ src/main/kotlin/lesson_18/lesson18_task2.kt
@@ -1,6 +1,6 @@
 package ru.fedorova.spring.lesson_18
 
-open class Dice(private val side: Int) {
+open class Dice(val side: Int) {
 
     open fun roll(): Int {
         return (1..side).random()
@@ -12,7 +12,7 @@ class FourSidesDice : Dice(4) {
 
     override fun roll(): Int {
         val result = super.roll()
-        println("Бросок 4-гранной игральной кости: $result")
+        println("Бросок $side-гранной игральной кости: $result")
         return result
     }
 
@@ -22,7 +22,7 @@ class SixSidesDice : Dice(6) {
 
     override fun roll(): Int {
         val result = super.roll()
-        println("Бросок 6-гранной игральной кости: $result")
+        println("Бросок $side-гранной игральной кости: $result")
         return result
     }
 
@@ -32,7 +32,7 @@ class EightSidesDice : Dice(8) {
 
     override fun roll(): Int {
         val result = super.roll()
-        println("Бросок 8-гранной игральной кости: $result")
+        println("Бросок $side-гранной игральной кости: $result")
         return result
     }
 
