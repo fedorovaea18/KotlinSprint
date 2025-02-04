7e9f92bdc1666d77e5a02c60dcff2ea785f94980
fedorovaea18 <fedorovaea18@yandex.ru>
2025-02-04 18:24:11 +0300

    KS-19-3 feat: added a class Spaceship and methods takeOff, land, shootAsteroid without their implementation

---
+++ src/main/kotlin/lesson_19/lesson19_task3.kt
@@ -0,0 +1,17 @@
+package ru.fedorova.spring.lesson_19
+
+class Spaceship {
+
+    fun takeOff() {
+        TODO("Здесь нужна информация о дополнительной логике")
+    }
+
+    fun land() {
+        //TODO здесь нужна информация о дополнительной логике
+    }
+
+    fun shootAsteroid() {
+        TODO()
+    }
+
+}
