dadedaaa15189d2ab40ea21691b1cbe8908d023b
fedorovaea18 <fedorovaea18@yandex.ru>
2025-02-05 10:58:07 +0300

    KS-19-4 feat: added enum class patrons and class Tank with methods of arming with a new type of patrons and shot

---
+++ src/main/kotlin/lesson_19/lesson19_task4.kt
@@ -0,0 +1,35 @@
+package ru.fedorova.spring.lesson_19
+
+enum class Patron(val damage: Int) {
+    BLUE(5),
+    GREEN(10),
+    RED(20),
+
+}
+
+class Tank {
+
+    fun loadPatron(patron: Patron) {
+        println("Патрон: $patron")
+    }
+
+    fun shoot(patron: Patron) {
+        println("Урон: ${patron.damage}")
+    }
+
+}
+
+fun main() {
+
+    val tank1 = Tank()
+
+    tank1.loadPatron(Patron.BLUE)
+    tank1.shoot(Patron.BLUE)
+
+    tank1.loadPatron(Patron.GREEN)
+    tank1.shoot(Patron.GREEN)
+
+    tank1.loadPatron(Patron.RED)
+    tank1.shoot(Patron.RED)
+
+}
